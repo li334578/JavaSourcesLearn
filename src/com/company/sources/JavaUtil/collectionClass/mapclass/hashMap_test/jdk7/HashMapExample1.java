af6ec3f45516fbb34ea4a6a61d2f180318af43eb
LiWenBo <liwenbo4922@163.com>
2021-01-23 21:56:43 +0800

    HashMap jdk1.7 并发修改异常

--- src/com/company/sources/JavaUtil/collectionClass/mapclass/hashMap_test/jdk7/HashMapExample1.java
+++ src/com/company/sources/JavaUtil/collectionClass/mapclass/hashMap_test/jdk7/HashMapExample1.java
@@ -2,6 +2,7 @@ package com.company.sources.JavaUtil.collectionClass.mapclass.hashMap_test.jdk7;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  * @author : LiWenBo
@@ -15,13 +16,22 @@ public class HashMapExample1 {
         list.add("hello");
         System.out.println(list.size());
         HashMap<String, String> hashMap = new HashMap<>();
-        hashMap.put("hello","world");
+        hashMap.put("hello", "world");
         // hash , key , value , bucketIndex
         // 头插法
         // table[bucketIndex] = new Entry(hash,key,value,table[bucketIndex]);
         // 初始化hashMap的容量为2的幂次方数
         // 17 -> 32 17-1 左移一位 32 除最高位的1 低位全置为0
         // key为null 放到hash表的0位置上 因为null无法计算hash值
-
+        // 并发修改异常modCount
+        for (String s : hashMap.keySet()) {
+            // 会导致并发修改异常
+            hashMap.remove(s);
+        }
+        Iterator<String> iterator = hashMap.keySet().iterator();
+        while (iterator.hasNext()) {
+            // 可以避免并发修改异常
+            iterator.remove();
+        }
     }
 }
