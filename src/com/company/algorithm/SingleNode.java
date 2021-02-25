950235b172b838e482a5c35bd8ffa42de3ced41b
LiWenBo <liwenbo4922@163.com>
2021-02-25 22:01:08 +0800

    单链表

---
+++ src/com/company/algorithm/SingleNode.java
@@ -0,0 +1,77 @@
+package com.company.algorithm;
+
+/**
+ * @author : LiWenBo
+ * @program : JavaSourcesLearn
+ * @description : 单链表节点
+ * @date : 2021-02-22 20:50:49
+ */
+public class SingleNode {
+
+    private int value;
+    private SingleNode nextSingleNode;
+
+    public SingleNode(int value) {
+        this.value = value;
+    }
+
+    /**
+     * 向链表末尾追加节点
+     *
+     * @param singleNode 节点
+     * @return this
+     */
+    public SingleNode appendSingleNode(SingleNode singleNode) {
+        SingleNode currentSingleNode = this;
+        while (currentSingleNode.next() != null) {
+            currentSingleNode = currentSingleNode.next();
+        }
+        currentSingleNode.nextSingleNode = singleNode;
+        return this;
+    }
+
+    /**
+     * 获取下一个节点
+     *
+     * @return 下一个节点
+     */
+    public SingleNode next() {
+        return this.nextSingleNode;
+    }
+
+    /**
+     * 判断当前节点是否是最后一个节点
+     *
+     * @return boolean
+     */
+    public boolean isLast() {
+        return nextSingleNode == null;
+    }
+
+    /**
+     * 当前节点后插入节点
+     *
+     * @param singleNode 插入的节点
+     */
+    public void afterInsert(SingleNode singleNode) {
+        singleNode.nextSingleNode = this.next();
+        this.nextSingleNode = singleNode;
+    }
+
+    /**
+     * 获取当前节点的值
+     *
+     * @return 获取当前节点的值
+     */
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "SingleNode{" +
+                "value=" + value +
+                ", nextSingleNode=" + nextSingleNode +
+                '}';
+    }
+}
---
+++ src/com/company/algorithm/SingleNodeTest.java
@@ -0,0 +1,27 @@
+package com.company.algorithm;
+
+/**
+ * @author : LiWenBo
+ * @program : JavaSourcesLearn
+ * @description : 单链表测试
+ * @date : 2021-02-22 20:51:45
+ */
+public class SingleNodeTest {
+    public static void main(String[] args) {
+        SingleNode singleNode0 = new SingleNode(0);
+        SingleNode singleNode1 = new SingleNode(2);
+        SingleNode singleNode2 = new SingleNode(4);
+        SingleNode singleNode3 = new SingleNode(6);
+        SingleNode singleNode4 = new SingleNode(8);
+        SingleNode singleNode5 = new SingleNode(10);
+        singleNode0.appendSingleNode(singleNode1).appendSingleNode(singleNode2)
+                .appendSingleNode(singleNode3).appendSingleNode(singleNode4).appendSingleNode(singleNode5);
+        System.out.println(singleNode0);
+        System.out.println(singleNode0.next().next());
+        System.out.println(singleNode0.next().next().getValue());
+
+        SingleNode singleNodeInsert = new SingleNode(-1);
+        singleNode0.next().next().afterInsert(singleNodeInsert);
+        System.out.println(singleNode0);
+    }
+}
