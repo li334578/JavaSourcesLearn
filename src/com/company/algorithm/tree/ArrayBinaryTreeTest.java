a31f999ecdaaa5f7b4ebed374ab851545e07e4b2
LiWenBo <liwenbo4922@163.com>
2021-03-13 20:43:32 +0800

    数组实现二叉树

---
+++ src/com/company/algorithm/tree/ArrayBinaryTreeTest.java
@@ -0,0 +1,15 @@
+package com.company.algorithm.tree;
+
+/**
+ * @author : LiWenBo
+ * @program : JavaSourcesLearn
+ * @description : 数组顺序存储二叉树测试方法
+ * @date : 2021-03-08 20:29:57
+ */
+public class ArrayBinaryTreeTest {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        ArrayBinaryTree arrayBinaryTree = new ArrayBinaryTree(nums);
+        arrayBinaryTree.frontShow();
+    }
+}
