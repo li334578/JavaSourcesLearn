f0462dec11c64cc7048bc1e86e66334cc4f81fcf
LiWenBo <liwenbo4922@163.com>
2021-03-21 19:45:16 +0800

    Hash表

---
+++ src/com/company/algorithm/hashTable/HashTable.java
@@ -0,0 +1,30 @@
+package com.company.algorithm.hashTable;
+
+import java.util.Arrays;
+
+/**
+ * @author : LiWenBo
+ * @program : JavaSourcesLearn
+ * @description : 哈希表实现
+ * @date : 2021-03-17 21:19:52
+ */
+public class HashTable {
+    private StudentInfo[] data = new StudentInfo[10];
+
+    public void put(StudentInfo studentInfo) {
+        data[studentInfo.hashCode()] = studentInfo;
+    }
+
+    public StudentInfo get(int age) {
+        StudentInfo studentInfo = new StudentInfo();
+        studentInfo.setAge(age);
+        return data[studentInfo.hashCode()];
+    }
+
+    @Override
+    public String toString() {
+        return "HashTable{" +
+                "data=" + Arrays.toString(data) +
+                '}';
+    }
+}
---
+++ src/com/company/algorithm/hashTable/HashTableTest.java
@@ -0,0 +1,27 @@
+package com.company.algorithm.hashTable;
+
+/**
+ * @author : LiWenBo
+ * @program : JavaSourcesLearn
+ * @description : 哈市表测试
+ * @date : 2021-03-17 21:23:57
+ */
+public class HashTableTest {
+    public static void main(String[] args) {
+        StudentInfo s1 = new StudentInfo(19, "熊大");
+        StudentInfo s2 = new StudentInfo(20, "熊二");
+        StudentInfo s3 = new StudentInfo(18, "张三");
+        StudentInfo s4 = new StudentInfo(17, "李四");
+        StudentInfo s5 = new StudentInfo(16, "王五");
+        StudentInfo s6 = new StudentInfo(15, "赵六");
+        HashTable hashTable = new HashTable();
+        hashTable.put(s1);
+        hashTable.put(s2);
+        hashTable.put(s3);
+        hashTable.put(s4);
+        hashTable.put(s5);
+        hashTable.put(s6);
+        System.out.println(hashTable.get(17));
+        System.out.println(hashTable);
+    }
+}
