2e2777819281f7386d0040c61236467d262f1679
LiWenBo <liwenbo4922@163.com>
2021-03-22 20:56:53 +0800

    Hash加密解密

---
+++ src/com/company/algorithm/huffmanCode/Node.java
@@ -0,0 +1,32 @@
+package com.company.algorithm.huffmanCode;
+
+/**
+ * @author : LiWenBo
+ * @program : JavaSourcesLearn
+ * @description : 节点
+ * @date : 2021-03-09 21:41:31
+ */
+public class Node implements Comparable<Node> {
+    Byte value;
+    int weight;
+    Node left;
+    Node right;
+
+    public Node(Byte value, int weight) {
+        this.value = value;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return o.weight - this.weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", weight=" + weight +
+                '}';
+    }
+}
