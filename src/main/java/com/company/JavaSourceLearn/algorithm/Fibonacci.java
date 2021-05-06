03aaf6200f4b3594f0b46ae1e0430f0f3ac595ba
LiWenBo <liwenbo4922@163.com>
2021-05-06 21:03:46 +0800

    Fibonacci

--- src/main/java/com/company/JavaSourceLearn/algorithm/Fibonacci.java
+++ src/main/java/com/company/JavaSourceLearn/algorithm/Fibonacci.java
@@ -1,5 +1,10 @@
 package com.company.JavaSourceLearn.algorithm;
 
+import cn.hutool.core.date.DateUtil;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 /**
  * @author : LiWenBo
  * @program : JavaSourcesLearn
@@ -23,10 +28,30 @@ public class Fibonacci {
         }
     }
 
+    public int recursionFibonacci2(int n) {
+        if (n <= 1) return n;
+        int first = 1;
+        int second = 1;
+        for (int i = 1; i < n - 1; i++) {
+            int sum = first + second;
+            first = second;
+            second = sum;
+        }
+        return second;
+    }
+
     public static void main(String[] args) {
         Fibonacci fun = new Fibonacci();
-        for (int i = 0; i < 10; i++) {
-            System.out.println(fun.recursionFibonacci(i));
-        }
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(fun.recursionFibonacci(i));
+//        }
+
+        System.out.println(LocalDateTime.now());
+        System.out.println(fun.recursionFibonacci(45));
+        System.out.println(LocalDateTime.now());
+        System.out.println("-------------------");
+        System.out.println(LocalDateTime.now());
+        System.out.println(fun.recursionFibonacci2(45));
+        System.out.println(LocalDateTime.now());
     }
 }
