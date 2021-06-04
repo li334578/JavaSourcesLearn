977f5b3e80cfe0204769eca2061744cf6504c205
liwenbo <liwenbo4922@163.com>
2021-06-04 15:46:09 +0800

    装饰器

---
+++ src/main/java/com/company/JavaSourceLearn/designPatterns2/creatorMode/design09/Battercake.java
@@ -0,0 +1,12 @@
+package com.company.JavaSourceLearn.designPatterns2.creatorMode.design09;
+
+/**
+ * @Author: liwenbo
+ * @Date: 2021年06月01日
+ * @Description: 煎饼抽象类
+ */
+public abstract class Battercake {
+    protected abstract String getMsg();
+
+    protected abstract int getPrice();
+}
