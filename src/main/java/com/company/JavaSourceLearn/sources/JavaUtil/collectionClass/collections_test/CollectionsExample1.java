f8bbe0fec277bb33f432ce63f94e87c0071b8b67
LiWenBo <liwenbo4922@163.com>
2021-07-28 20:24:10 +0800

    CollectionsExample1

---
+++ src/main/java/com/company/JavaSourceLearn/sources/JavaUtil/collectionClass/collections_test/CollectionsExample1.java
@@ -0,0 +1,13 @@
+package com.company.JavaSourceLearn.sources.JavaUtil.collectionClass.collections_test;
+
+/**
+ * @author : LiWenBo
+ * @program : JavaSourcesLearn
+ * @description : Collections
+ * @date : 2021-01-14 20:27:35
+ */
+public class CollectionsExample1 {
+    public static void main(String[] args) {
+
+    }
+}
