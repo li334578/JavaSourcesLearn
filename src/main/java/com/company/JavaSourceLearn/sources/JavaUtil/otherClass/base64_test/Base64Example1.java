06fd5887fe84a75bf3e71e2a27fdff86fbada2f0
LiWenBo <liwenbo4922@163.com>
2021-07-29 19:43:45 +0800

    Base64Example1

---
+++ src/main/java/com/company/JavaSourceLearn/sources/JavaUtil/otherClass/base64_test/Base64Example1.java
@@ -0,0 +1,13 @@
+package com.company.JavaSourceLearn.sources.JavaUtil.otherClass.base64_test;
+
+/**
+ * @author : LiWenBo
+ * @program : JavaSourcesLearn
+ * @description : Base64
+ * @date : 2021-01-18 21:23:10
+ */
+public class Base64Example1 {
+    public static void main(String[] args) {
+
+    }
+}
