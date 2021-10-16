1f7e3ec83e1b209955e0468518a0778aa41bbfdd
LiWenBo <liwenbo4922@163.com>
2021-10-16 21:03:05 +0800

    Copier

---
+++ src/main/java/com/company/JavaSourceLearn/sources/functionalInterface/stream_test/test1.java
@@ -0,0 +1,25 @@
+package com.company.JavaSourceLearn.sources.functionalInterface.stream_test;
+
+/**
+ * @author : LiWenBo
+ * @program : JavaSourcesLearn
+ * @description :
+ * @date : 2021-10-16 17:44:50
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println();
+        System.out.println("***");
+        System.out.println();
+        System.out.println("*****");
+        System.out.println();
+        System.out.println("*******");
+        System.out.println();
+        System.out.println("*****");
+        System.out.println();
+        System.out.println("***");
+        System.out.println();
+        System.out.println("*");
+    }
+}
